365b3b90f777cf944f6a86deff84e6ae9f62884b
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-05-26 12:57:17 -0700

    node12
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- api/handler.js
+++ api/handler.js
@@ -1,5 +1,12 @@
-"use strict"
+'use strict'
 
-module.exports = async (context, callback) => {
-    return {status: "done"}
+module.exports = async (event, context) => {
+  const result = {
+    'status': 'Received input: ' + JSON.stringify(event.body)
+  }
+
+  return context
+    .status(200)
+    .succeed(result)
 }
+
